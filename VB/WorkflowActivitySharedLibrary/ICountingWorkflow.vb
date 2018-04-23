02b14c12ddba3b11de919ea018f8e273838a3a34
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WorkflowActivitySharedLibrary/ICountingWorkflow.vb
@@ -0,0 +1,15 @@
+﻿'Refer to the http://community.devexpress.com/blogs/eaf/archive/2011/08/24/xaf-workflow-persistence-storage.aspx blog post for more details.
+
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Linq
+Imports System.ServiceModel
+Imports System.Collections.Generic
+
+Namespace WorkflowActivitySharedLibrary
+	<ServiceContract> _
+	Public Interface ICountingWorkflow
+		<OperationContract(IsOneWay := True)> _
+		Sub start()
+	End Interface
+End Namespace
